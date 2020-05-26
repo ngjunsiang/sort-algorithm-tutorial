93f2c7eadb92f0a842603274297320929042fdb3
JS Ng <ng_jun_siang@moe.edu.sg>
2020-05-26 02:28:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+def bubbleSort(array):
+    '''
+    Author: 
+    Date: 
+
+    Sorts array elements in ascending order (smallest to largest)
+    using bubble sort algorithm.
+    '''
+    pass
+
+def insertionSort(array):
+    '''
+    Author: 
+    Date: 
+
+    Sorts array elements in ascending order (smallest to largest)
+    using insertion sort algorithm.
+    '''
+    pass
+
+def mergeSort(array):
+    '''
+    Author: 
+    Date: 
+
+    Sorts array elements in ascending order (smallest to largest)
+    using merge sort algorithm.
+    '''
+    pass
+
+def quickSort(array):
+    '''
+    Author: 
+    Date: 
+
+    Sorts array elements in ascending order (smallest to largest)
+    using quick sort algorithm.
+    '''
+    pass
