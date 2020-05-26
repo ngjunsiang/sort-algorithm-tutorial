1e6ba20cb2feb3b827e88bc3c19fd609d17b01c6
JS Ng <ng_jun_siang@moe.edu.sg>
2020-05-26 02:33:44 +0000

    add tests, created test function template

---
+++ tests.py
@@ -0,0 +1,15 @@
+def testSort(sortFunction, **kwargs):
+    '''
+    Author: 
+    Date: 
+
+    Tests to verify that sort function works correctly.
+    
+    Requirements:
+    1. Output array has same number of elements as input array
+    2. Each element in the output array is smaller than or equal to the
+       next element
+    3. Each element in the input must be in the output.
+       There must be no elements in the output that are not in the input.
+    '''
+    pass
