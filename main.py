5f402c6e7dd3321fba388678ab86e6559a0eafaa
JS Ng <ng_jun_siang@moe.edu.sg>
2020-06-10 08:33:30 +0000

    basic implementation with optimisations

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ def bubbleSort(array):
     if len(array) == 0:
         return array
     sorted_index = len(array) - 1
-    while sorted_index > 0:
+    while not noswaps and sorted_index > 0:
+        noswaps = True
         for i in range(1, sorted_index + 1):
             if array[i-1] > array[i]:
                 array[i-1], array[i] = array[i], array[i-1]
+                noswaps = False
+        # return array early if no swaps made (i.e. sorted)
+        if noswaps:
+            return array
         sorted_index -= 1
     return array
 
@@ -28,23 +33,25 @@ def insertionSort(array):
     Sorts array elements in ascending order (smallest to largest)
     using insertion sort algorithm.
     '''
+    # Use a function to exit for loop early
+    def first_index(array, el):
+        '''Returns the first index of array that is <= el'''
+        for i in range(0, len(array)):
+            if el < array[i]:
+                return i
+        return None  # if no index found
+
     size = len(array) - 1
-    sorted_index = 0
-    while sorted_index < size:
+    for sorted_index in range(size):
         el = array.pop(sorted_index + 1)
-        inserted = False
-        i = 0
-        while i <= sorted_index:
-            if not inserted and el <= array[i]:
-                array.insert(i, el)
-                inserted = True
-            i += 1
-        if not inserted:
+        insert_index = first_index(array[:sorted_index + 1], el)
+        if insert_index is None:
             if sorted_index >= size:
                 array.append(el)
             else:
                 array.insert(sorted_index + 1, el)
-        sorted_index += 1
+        else:
+            array.insert(insert_index, el)
     return array
 
 
@@ -57,14 +64,24 @@ def mergeSort(array):
     using merge sort algorithm.
     '''
     def merge(left, right):
-        array = []
-        while len(left) > 0 and len(right) > 0:
-            if left[0] <= right[0]:
-                el = left.pop(0)
+        # preallocate array to minimise array appending
+        array = [None] * (len(left) + len(right))
+        l_size, r_size = len(left), len(right)
+        a = l = r = 0  # array indices
+        while l < l_size and r < r_size:
+            if left[l] <= right[r]:
+                array[a] = left[l]
+                a += 1
+                l += 1
             else:
-                el = right.pop(0)
-            array.append(el)
-        return array + left + right
+                array[a] = right[r]
+                a += 1
+                r += 1
+        if l < l_size:
+            array[a:] = left[l:]
+        if r < r_size:
+            array[a:] = right[r:]
+        return array
     
     if len(array) <= 1:
         return array
@@ -87,17 +104,13 @@ def quickSort(array):
     Sorts array elements in ascending order (smallest to largest)
     using quick sort algorithm.
     '''
+    # speed up creation of ltearray and gtarray
+    # using list comprehensions (avoid append)
     if len(array) <= 1:
         return array
-    # pick last element as pivot
     pivot = array[-1]
-    ltearray = []
-    gtarray = []
-    for el in array[:-1]:
-        if el <= pivot:
-            ltearray.append(el)
-        else:
-            gtarray.append(el)
+    ltearray = [el for el in array[:-1] if el <= pivot]
+    gtarray = [el for el in array[:-1] if el > pivot]
     ltearray = quickSort(ltearray)
     gtarray = quickSort(gtarray)
     return ltearray + [pivot] + gtarray
